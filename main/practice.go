9dc18545bca2bed86ed7cf90614f44ba9c177093
Shashank Bhardwaj <shashank123bhardwaj@gmail.com>
2020-03-13 17:22:24 +0530

    practice question

--- main/practice.go
+++ main/practice.go
@@ -2,8 +2,69 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Hello World!!")
+	var (
+		testCases     int
+		inputStacks   []string
+		inputStack    string
+		lenStack      int
+		maneuverCount int
+	)
+	fmt.Println("Enter the Test Cases \n")
+	fmt.Scanf("%d", &testCases)
+	fmt.Printf("\nEnter %d input Strings\n", testCases)
+	for i := 0; i < testCases; i++ {
+		fmt.Scanf("%s", &inputStack)
+		lenStack = len(inputStack)
+		stringStack := strings.Split(inputStack, "")
+		fmt.Println("\n", stringStack, lenStack, "\n")
+		maneuverCount = maneuver(stringStack, lenStack-1, 0)
+
+		inputStacks = append(inputStacks, inputStack)
+	}
+	fmt.Println(inputStacks, maneuverCount)
+}
+
+func maneuver(inputStack []string, downFaceCount, maneuverCount int) int {
+
+	fmt.Println("inside maneuver \n", inputStack, downFaceCount, maneuverCount, "\n")
+	fmt.Println("before calling findDownfacecount \n", inputStack, downFaceCount, "\n")
+	downFaceCount = findNextDownFaceCount(inputStack, downFaceCount)
+	fmt.Println("after calling findDownfacecount \n", inputStack, downFaceCount, maneuverCount, "\n")
+	if downFaceCount >= 0 {
+		maneuverCount = maneuverCount + 1
+		for j := downFaceCount; j >= 0; j-- {
+			fmt.Println("before calling revertFace\n", inputStack, j, "\n")
+			inputStack = revertFace(inputStack, j)
+			fmt.Println("after calling revertFace\n", inputStack, j, "\n")
+		}
+		fmt.Println("before calling maneuver \n", inputStack, downFaceCount, maneuverCount, "\n")
+		maneuverCount = maneuver(inputStack, downFaceCount-1, maneuverCount)
+	}
+	fmt.Printf("ManeuverCount %d", maneuverCount)
+	return maneuverCount
+
+}
+
+func findNextDownFaceCount(inputString []string, previousDownFaceCount int) int {
+
+	for j := previousDownFaceCount; j >= 0; j-- {
+		if inputString[j] == "-" {
+			return j
+		}
+	}
+
+	return -1
+}
+
+func revertFace(inputString []string, count int) []string {
+	if inputString[count] == "-" {
+		inputString[count] = "+"
+	} else {
+		inputString[count] = "-"
+	}
+	return inputString
 }
---
+++ main/stringhandle.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	stringArr := []string{"4", "11", "3", "5", "-10", "3", "-5", "-8", "9", "12", "-13", "-14", "15", "20", "-6"}
+	var result string
+	var maxElement, secondMaxElement, val string
+	for _, stringNum := range stringArr {
+		fmt.Println(maxElement, stringNum)
+		val = FindMaxElement(maxElement, stringNum)
+		if val == stringNum {
+			secondMaxElement = maxElement
+			maxElement = val
+		} else {
+			secondMaxElement = FindMaxElement(secondMaxElement, stringNum)
+		}
+
+	}
+
+	if secondMaxElement == "" {
+		result = "-1"
+	}
+
+	result = secondMaxElement
+
+	fmt.Println("Second Maximum Element", result)
+
+}
+
+func FindMaxElement(var1, var2 string) string {
+
+	if var1 == "" {
+		return var2
+	} else if var2 == "" {
+		return var1
+	}
+
+	isnegative := false
+	if var1[0] == '-' && var2[0] != '-' {
+		return var2
+	} else if var1[0] != '-' && var2[0] == '-' {
+		return var1
+	} else if var1[0] == '-' && var2[0] == '-' {
+		isnegative = true
+	}
+
+	var1length := len(var1)
+	var2length := len(var2)
+
+	if var1length > var2length {
+		if isnegative {
+			return var2
+		}
+		return var1
+	}
+	if var2length > var1length {
+		if isnegative {
+			return var1
+		}
+		return var2
+	} else if var1length == var2length {
+		for i := 0; i < var1length; i++ {
+			if var1[i] > var2[i] {
+				if isnegative {
+					return var2
+				}
+				return var1
+			} else if var1[i] < var2[i] {
+				if isnegative {
+					return var1
+				}
+				return var2
+			}
+		}
+	}
+	return var1
+}
